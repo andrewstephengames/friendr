c1884bb2bfbd661a7f2b606d282e44397e11aa9e
andrewstephengames <andrewstephengames999@gmail.com>
2024-06-27 10:34:33 +0300

    Implement MongoDB connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require ('express');
+const mongoose = require('mongoose');
+const secrets = require('./secrets')
 const usersRouter = require ('./routers/users.router');
 const postsRouter = require ('./routers/posts.router');
 
@@ -16,4 +18,9 @@ app.use('/posts', postsRouter);
 
 app.listen(port, () => {
   console.log(`[INFO]: Server is running on port ${port}`);
+  mongoose.connect(`mongodb+srv://andrew:${secrets}@cluster0.4xlt9av.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(() => {
+    console.log ('[INFO]: Connected to MongoDB')
+  }).catch ((err) => {
+    console.log (`[ERROR]: Unable to connect to the database: ${err}`)
+  })
 })
